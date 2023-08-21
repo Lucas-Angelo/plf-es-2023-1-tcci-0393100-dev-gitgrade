3620c4430a351a84ddca0b9732d22fa28b2abde1
Lucas Ângelo <lcs2001_@hotmail.com>
2023-08-20 22:28:05 -0300

    feat(jobscheduler-service): add pullrequest env action

--- Codigo/run-docker.sh
+++ Codigo/run-docker.sh
@@ -39,15 +39,6 @@ handle_interrupt() {
     exit
 }
 
-get_env_suffix() {
-    case "$1" in
-        -dev) echo "development" ;;
-        -test) echo "test" ;;
-        -prod) echo "production" ;;
-        *) echo "Usage: $0 {-dev|-test|-prod} [--branch branchName] [--d]"; exit 1 ;;
-    esac
-}
-
 check_detached_mode() {
     for arg in "$@"; do
         if [[ "$arg" == "--d" ]]; then
@@ -58,48 +49,19 @@ check_detached_mode() {
     echo ""
 }
 
-get_valid_directories() {
-    directories=("./jobscheduler-service/env" "./supportplatform-service/env" "./supportplatform-web/env")
-    valid_dirs=()
-
-    for dir in "${directories[@]}"; do
-        if [ -d "$dir" ] && { find "$dir" -type f -name "*.env" -print -quit | grep -q . ; } || \
-                          { find "$dir" -type f -name "*.env.$1" -print -quit | grep -q . ; }; then
-            valid_dirs+=("$dir")
-        fi
-    done
-
-    echo "${valid_dirs[@]}"
-}
-
 build_docker_command() {
     BRANCH="$1"
-    ENV_SUFFIX="$2"
-    DETACHED="$3"
-    VALID_DIRS=("$@")
-
-    cmd="docker-compose -p gitgrade_$BRANCH"
+    DETACHED="$2"
     
-    for dir in "${VALID_DIRS[@]:3}"; do
-        for file in $(find "$dir" -type f -name "*.env"); do
-            cmd="$cmd --env-file $file"
-        done
-        for specific in $(find "$dir" -type f -name "*.env.$ENV_SUFFIX"); do
-            cmd="$cmd --env-file $specific"
-        done
-    done
-
-    cmd="$cmd up $DETACHED"
+    cmd="docker-compose --env-file .env -p gitgrade_$BRANCH up $DETACHED"
     echo "$cmd"
 }
 
 check_git_installed
 BRANCH_NAME=$(sanitize_branch_name $(get_branch_name "$@"))
 trap 'handle_interrupt' INT
-ENV_SUFFIX=$(get_env_suffix "$1")
 DETACHED=$(check_detached_mode "$@")
-VALID_DIRS=($(get_valid_directories "$ENV_SUFFIX"))
-CMD=$(build_docker_command "$BRANCH_NAME" "$ENV_SUFFIX" "$DETACHED" "${VALID_DIRS[@]}")
+CMD=$(build_docker_command "$BRANCH_NAME" "$DETACHED")
 
 echo "Running: $CMD"
 eval "$CMD"
