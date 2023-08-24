7831ccf785567065fbc789319d98152fd606a398
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2023-08-24 19:43:26 -0300

    build(web): add config to get vite port from .env or defaults to 3000

--- Codigo/supportplatform-web/vite.config.ts
+++ Codigo/supportplatform-web/vite.config.ts
@@ -1,19 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        checker({
-            typescript: {
-                tsconfigPath: "./tsconfig.json",
-            },
-            eslint: {
-                lintCommand: "eslint . --ext ts,tsx",
-            },
-        }),
-    ],
-    envDir: "./env",
+export default defineConfig(({ mode }) => {
+    const envDir = path.resolve(process.cwd(), "env");
+    process.env = Object.assign(process.env, loadEnv(mode, envDir, ""));
+    const { VITE_PORT } = process.env;
+    const port = Number(VITE_PORT) || 3000;
+
+    return {
+        plugins: [
+            react(),
+            checker({
+                typescript: {
+                    tsconfigPath: "./tsconfig.json",
+                },
+                eslint: {
+                    lintCommand: "eslint . --ext ts,tsx",
+                },
+            }),
+        ],
+        envDir: "./env",
+        server: {
+            port,
+        },
+    };
 });
