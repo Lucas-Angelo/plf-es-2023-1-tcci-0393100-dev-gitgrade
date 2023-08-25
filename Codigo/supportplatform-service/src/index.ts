77f9828ec2eb783a20cd716d8b81289535f2f83b
Lucas Ângelo <lcs2001_@hotmail.com>
2023-08-24 21:59:47 -0300

    refactor(supportplatform-service): remove unused imports

--- Codigo/supportplatform-service/src/index.ts
+++ Codigo/supportplatform-service/src/index.ts
@@ -3,10 +3,8 @@ import express from "express";
 import { RegisterRoutes } from "./swagger/routes";
 
 import swaggerUi from "swagger-ui-express";
-import EnvConfig from "./config/EnvConfig";
-import Database from "./database";
-import AppError from "./error/AppError";
 import { ValidateError } from "tsoa";
+import AppError from "./error/AppError";
 
 const app = express();
 
