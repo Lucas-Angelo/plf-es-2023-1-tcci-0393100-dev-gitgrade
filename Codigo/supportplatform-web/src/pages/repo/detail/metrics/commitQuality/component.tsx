68ffeb9ce0f2251b290d220dd5fc74892c012d82
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2023-09-16 18:59:34 -0300

    feat(repo-metrics): add info of ignored commits on commit metrics chart page

--- Codigo/supportplatform-web/src/pages/repo/detail/metrics/commitQuality/component.tsx
+++ Codigo/supportplatform-web/src/pages/repo/detail/metrics/commitQuality/component.tsx
@@ -1,7 +1,8 @@
-import { Box, Text } from "@primer/react";
+import { Box, Text, Tooltip, Octicon } from "@primer/react";
 import { usePageCommitQualityMetricsData } from "./hooks/usePageCommitQualityMetricsData";
 import CommitQualityChart from "./components/commitQualityChart";
 import CommitQualityIndicators from "./components/commitQualityIndicators";
+import { InfoIcon } from "@primer/octicons-react";
 
 export default function RepositoryCommitQualityMetricsPage() {
     const commitQualityMetrics = usePageCommitQualityMetricsData();
@@ -9,6 +10,15 @@ export default function RepositoryCommitQualityMetricsPage() {
         <Box>
             <Text sx={{ fontSize: 20, fontWeight: 500, ml: [0, 2, 4, 6] }}>
                 Qualidade (tamanho) de descrição de commits por contribuidor
+                <Tooltip
+                    wrap
+                    aria-label='As descrições de commit que inciam com "Merge branch", "Merge remote-tracking branch" e "Merge pull request" são ignoradas, por serem descrições comumente geradas automaticamente pelo GitHub'
+                >
+                    <Octicon
+                        sx={{ mb: 1, ml: 2 }}
+                        icon={InfoIcon}
+                    />
+                </Tooltip>
             </Text>
 
             <Box
