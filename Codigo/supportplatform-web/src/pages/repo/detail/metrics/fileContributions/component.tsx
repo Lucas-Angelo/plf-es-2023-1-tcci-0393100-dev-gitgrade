64d9b568deb804774db4f1f58020f1730d55adf8
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2023-10-05 20:46:29 -0300

    feat(repo-metrics): add details description of the file changes chart and no contributor group

--- Codigo/supportplatform-web/src/pages/repo/detail/metrics/fileContributions/component.tsx
+++ Codigo/supportplatform-web/src/pages/repo/detail/metrics/fileContributions/component.tsx
@@ -2,9 +2,15 @@ import { Box, Text } from "@primer/react";
 import { useFileContributionsMetricsPageData } from "./hooks/useFileContributionsMetricsPageData";
 import FileContributionsIndicators from "./components/fileContributionsIndicators";
 import FileContributionsChart from "./components/fileContributionsChart";
+import ChartDetails from "../components/chartDetails";
+import NoContributorDetails from "../components/noContributorDetails";
 
 export default function RepositoryFileContributionsMetricsPage() {
     const fileChangeMetrics = useFileContributionsMetricsPageData();
+    const hasNoContributorMetrics =
+        fileChangeMetrics?.fileChangesPerContributor.some(
+            (item) => !item.contribuitor
+        );
     return (
         <Box>
             <Text sx={{ fontSize: 20, fontWeight: 500, ml: [0, 2, 4, 6] }}>
@@ -26,6 +32,18 @@ export default function RepositoryFileContributionsMetricsPage() {
                     <FileContributionsChart
                         fileChangeMetrics={fileChangeMetrics}
                     />
+                )}
+
+                <ChartDetails sx={{ mt: 8 }}>
+                    Esse gráfico soma todos os arquivos manipulados por cada
+                    contribuidor. Os arquivos considerados no somatório podem
+                    ter sido adicionados, removidos, deletados, alterados ou
+                    renomeados. São filtrados os arquivos pertencentes ao
+                    commits que foram realizados entre a data selecionada.
+                </ChartDetails>
+
+                {hasNoContributorMetrics && (
+                    <NoContributorDetails sx={{ mt: 4 }} />
                 )}
             </Box>
         </Box>
